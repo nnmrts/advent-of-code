72e49bb071037fdea03ef94262af18623b284953
Nano Miratus <nanomiratus@gmail.com>
2026-01-05 04:00:08 +0100

    simplify point-on-segment

--- years/2025/09/2/solution/point-in-or-on-polygon/point-on-segment.js
+++ years/2025/09/2/solution/point-in-or-on-polygon/point-on-segment.js
@@ -7,23 +7,20 @@ const pointOnSegment = (point, segment) => {
 	const [x, y] = point;
 	const [[x1, y1], [x2, y2]] = segment;
 
-	if (x1 === x2) {
-		return (
-			(x === x1) &&
-			(y >= Math.min(y1, y2)) &&
-			(y <= Math.max(y1, y2))
-		);
-	}
-
-	if (y1 === y2) {
-		return (
-			(y === y1) &&
-			(x >= Math.min(x1, x2)) &&
-			(x <= Math.max(x1, x2))
-		);
-	}
-
-	return false;
+	return (
+		(
+			x1 === x2 &&
+			x === x1 &&
+			y >= Math.min(y1, y2) &&
+			y <= Math.max(y1, y2)
+		) ||
+		(
+			y1 === y2 &&
+			y === y1 &&
+			x >= Math.min(x1, x2) &&
+			x <= Math.max(x1, x2)
+		)
+	);
 };
 
 export default pointOnSegment;
