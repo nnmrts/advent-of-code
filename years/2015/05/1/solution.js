cfa171ac403b50c283dbb23b6000504140283196
Nano Miratus <nanomiratus@gmail.com>
2026-01-07 00:00:16 +0100

    optimize 2015/05/1

--- years/2015/05/1/solution.js
+++ years/2015/05/1/solution.js
@@ -1,6 +1,6 @@
 /* eslint-disable regexp/no-super-linear-backtracking, security/detect-unsafe-regex -- santa isn't a hacker */
 import list from "../_common/list.js";
 
-const matches = list.matchAll(/^(?:a(?!b)|(?<!a)b|c(?!d)|(?<!c)d|[e-or-wz]|p(?!q)|(?<!p)q|x(?!y)|(?<!x)y)*?(?=(?:(?:a(?!b)|(?<!a)b|c(?!d)|(?<!c)d|[e-or-wz]|p(?!q)|(?<!p)q|x(?!y)|(?<!x)y)*?[aeiou](?:a(?!b)|(?<!a)b|c(?!d)|(?<!c)d|[e-or-wz]|p(?!q)|(?<!p)q|x(?!y)|(?<!x)y)*?){3})(?=(?:a(?!b)|(?<!a)b|c(?!d)|(?<!c)d|[e-or-wz]|p(?!q)|(?<!p)q|x(?!y)|(?<!x)y)*?(?<first>[a-z])\k<first>)(?:a(?!b)|(?<!a)b|c(?!d)|(?<!c)d|[e-or-wz]|p(?!q)|(?<!p)q|x(?!y)|(?<!x)y)+$/gmv);
+const matches = list.matchAll(/^(?=(?:.*?[aeiou].*?){3})(?=.*?(?<first>[a-z])\k<first>)(?:(?!ab|cd|pq|xy).)+$/gmv);
 
 console.info([...matches].length);
