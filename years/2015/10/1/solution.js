ac9e93f0f547e1632d1f485dc461c79b6180dbcf
Nano Miratus <nanomiratus@gmail.com>
2026-01-10 18:26:15 +0100

    solve 2015/10

---
+++ years/2015/10/1/solution.js
@@ -0,0 +1,20 @@
+import sequence from "../_common/sequence.js";
+
+const times = 40;
+
+const result = Array.from({ length: times })
+	.reduce(
+		(previousValue) => {
+			const runs = [
+				...previousValue
+					.match(/(?<number>.)\k<number>*/gv)
+			];
+
+			return runs
+				.map((run) => `${run.length}${run[0]}`)
+				.join("");
+		},
+		sequence
+	);
+
+console.info(result.length);
---
+++ years/2015/10/_common/sequence.js
@@ -0,0 +1,11 @@
+import getInput from "../../../_common/get-input.js";
+
+/**
+ * @typedef {`"${string}"`} InputLine
+ */
+
+const input = await getInput();
+
+const sequence = input.trim();
+
+export default sequence;
