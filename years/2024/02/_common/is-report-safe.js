0222bf4baa3a16e5856ee7839bc91c86ba7d17cd
Nano Miratus <nanomiratus@gmail.com>
2026-01-26 04:37:05 +0100

    fix 2024/02

--- years/2024/02/_common/is-report-safe.js
+++ years/2024/02/_common/is-report-safe.js
@@ -2,15 +2,42 @@ const maximumSafeDistance = 3;
 
 /**
  *
+ * @param {object} options
+ * @param {"up"|"down"} options.direction
+ * @param {number} options.level
+ * @param {number} options.levelAfter
+ */
+const isLevelSafe = ({
+	direction,
+	level,
+	levelAfter
+}) => {
+	const difference = level - levelAfter;
+
+	const absoluteDifference = Math.abs(difference);
+
+	return !(
+		absoluteDifference === 0 ||
+		absoluteDifference > maximumSafeDistance ||
+		(
+			(difference < 0 && direction !== "up") ||
+			(difference > 0 && direction !== "down")
+		)
+	);
+};
+
+/**
+ *
  * @param {readonly number[]} report
  * @param {number} allowedNumberOfErrors
  */
 const isReportSafe = (report, allowedNumberOfErrors = 0) => {
+	/**
+	 * @type {"up"|"down"|undefined}
+	 */
 	let direction;
 
 	let result = true;
-
-	let numberOfErrors = 0;
 
 	for (const [levelIndex, level] of report.entries()) {
 		if (levelIndex === report.length - 1) {
@@ -23,29 +50,36 @@ const isReportSafe = (report, allowedNumberOfErrors = 0) => {
 
 		const difference = level - levelAfter;
 
-		const absoluteDifference = Math.abs(difference);
+		if (direction === undefined) {
+			direction = difference < 0 ? "up" : "down";
+		}
 
 		if (
-			absoluteDifference === 0 ||
-			absoluteDifference > maximumSafeDistance ||
-			(
-				direction !== undefined &&
-				(
-					(difference < 0 && direction !== "up") ||
-					(difference > 0 && direction !== "down")
-				)
-			)
+			!isLevelSafe({
+				direction,
+				level,
+				levelAfter
+			})
 		) {
-			numberOfErrors += 1;
-
-			if (numberOfErrors > allowedNumberOfErrors) {
+			if (allowedNumberOfErrors === 0) {
 				result = false;
 				break;
 			}
-		}
+			else {
+				result = report
+					.some((innerLevel, index) => {
+						const dampenedReport = [
+							...report
+								.slice(0, index),
+							...report
+								.slice(index + 1)
+						];
 
-		if (direction === undefined) {
-			direction = difference < 0 ? "up" : "down";
+						return isReportSafe(dampenedReport, allowedNumberOfErrors - 1);
+					});
+
+				break;
+			}
 		}
 	}
 
